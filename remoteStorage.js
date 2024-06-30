fe54d935c2d9fd627e6241d4a7ec319c1318dd09
Tarik Bozüslü <boz.tarik38@gmail.com>
2024-06-30 02:17:00 +0200

    fetch:post second commit

--- remoteStorage.js
+++ remoteStorage.js
@@ -1,6 +1,6 @@
 function onloadFunc() {
     console.log("test");
-    loadData("/name");
+    postData("/name", {"banana": "rama" })
 }
 
 const BASE_URL = "https://remotestorage-3f179-default-rtdb.europe-west1.firebasedatabase.app/";
@@ -8,5 +8,18 @@ const BASE_URL = "https://remotestorage-3f179-default-rtdb.europe-west1.firebase
 async function loadData(path=""){
   let response = await fetch(BASE_URL + path + ".json");
   let responseToJson = await response.json();
-  console.log(responseToJson);
+  
+}
+
+async function postData(path="",data={}){
+    let response = await fetch(BASE_URL + path + ".json", {
+    method: "POST" ,
+    header: {
+        "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data)
+
+    });
+
+    let responseToJson = await response.json();
 }
